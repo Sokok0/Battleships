6b45cd2dbe9218f68cba62e8c743bdabe27cdd77
Sokok0 <148645480+Sokok0@users.noreply.github.com>
2024-03-25 20:28:23 +0100

    Add files via upload

---
+++ Battleship22/Battleships/Battleships/Playfield.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    internal class Playfield
+    {
+        public string[,] field = new string[10, 10];
+
+        public string[,] enemyField = new string[10, 10];
+
+        public void PrepareFields()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    field[i, j] = " ";
+                    enemyField[i, j] = " ";
+                }
+            }
+        }
+        public void showField()
+        {
+            Console.WriteLine("     | A | B | C | D | E | F | G | H | I | J ");
+            Console.WriteLine("-----+---+---+---+---+---+---+---+---+---+---");
+            int num = 1;
+            for (int i = 0; i < field.GetLength(0); i++)
+            {
+                if (num == 10)
+                {
+                    Console.Write(" " + num);
+                }
+                else
+                {
+                    Console.Write("  " + num);
+                }
+
+                for (int j = 0; j < field.GetLength(1); j++)
+                {
+
+
+
+                    Console.Write("  |" + field[i, j]);
+
+                }
+                num++;
+                Console.WriteLine("\n-----+---+---+---+---+---+---+---+---+---+---");
+
+
+            }
+            Console.WriteLine("Kliknij aby kontynuować :3");
+            Console.ReadLine();
+        }
+
+        public void showEnemyField()
+        {
+            Console.WriteLine("    | A | B | C | D | E | F | G | H | I | J ");
+            Console.WriteLine("----+---+---+---+---+---+---+---+---+---+---");
+            int num = 1;
+            for (int i = 0; i < enemyField.GetLength(0); i++)
+            {
+                if (num == 10)
+                {
+                    Console.Write(num);
+                }
+                else
+                {
+                    Console.Write(" " + num);
+                }
+
+                for (int j = 0; j < enemyField.GetLength(1); j++)
+                {
+
+
+
+                    Console.Write("  |" + enemyField[i, j]);
+
+                }
+                num++;
+                Console.WriteLine("\n----+---+---+---+---+---+---+---+---+---+---");
+
+
+            }
+        }
+
+        public void ChooseYourShip(Player player)
+        {
+            int howMany1 = 4;
+            int howMany2 = 3;
+            int howMany3 = 2;
+            int howMany4 = 1;
+            for (int i = 0; i < 4; i++)
+            {
+
+                string choice;
+                Console.WriteLine("Wybierz statek: ");
+                Console.WriteLine(" +---------------------+\n " +
+                    "| 1. Jednomasztowiec  |\n" +
+                    " | 2. Dwumasztowiec    |\n" +
+                    " | 3. Trzymasztowiec   |\n" +
+                    " | 4. Czteromasztowiec |\n" +
+                    " +---------------------+\n ");
+                choice = Console.ReadLine();
+
+                int TryPlacingShip(int index, int length, int howMany)
+                {
+                    
+                    if (howMany == 0)
+                    {
+                        Console.WriteLine("Nie masz więcej takich statków!");
+                        i--;
+                        return 0;
+                    }
+                    else
+                    {
+                        if(length == 1)
+                        {
+                            player.ships[index - 1].placeShip1(player);
+                        }
+                        else
+                        {
+                            player.ships[index - 1].placeShipLenght2(length, player);
+                        }
+                        
+                        player.ships[index - 1].blockNearLocations(player.field);
+                        player.showField();
+                        index--;
+                       
+
+                    }
+                    return 1;
+                }
+
+                if (choice == "1")
+                {
+                    howMany1 -= TryPlacingShip(howMany1, 1, howMany1);
+                    Console.Write($"Pozostałe statki jednomasztowe:  {howMany1}\n");
+                }
+
+                else if (choice == "2")
+                {
+                    howMany2 -= TryPlacingShip((howMany2+4), 2, howMany2);
+                    Console.Write($"Pozostałe statki dwumasztowe:  {howMany2}\n");
+                }
+
+                else if (choice == "3")
+                {
+                    howMany3 -= TryPlacingShip((howMany3 + 4 + 3), 3, howMany3);
+                    Console.Write($"Pozostałe statki trzymasztowe:  {howMany3}\n");
+
+                }
+
+                else if (choice == "4")
+                {
+                    howMany4 -= TryPlacingShip((howMany4 + 9), 4, howMany4);
+                    Console.Write($"Pozostałe statki czterowasztowe:  {howMany4}\n");
+
+                }
+                else
+                {
+                    Console.WriteLine("głuptasek - złe cyfery");
+                    i--;
+                    
+                }
+            }
+
+        }
+
+
+    }
+}
---
+++ Battleship22/Battleships/Battleships/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleships
+{
+    internal class Program
+    {
+        public static void Gameover(Player playerWon, Player playerLoser)
+        {
+
+            Console.Clear();
+            playerWon.winNumber++;
+            Console.WriteLine("GRACZ NR " + playerWon.playerNumber + " WYGRAŁ!!!");
+            Console.WriteLine("Gracz nr " + playerWon.playerNumber + " ma wygranych: " + playerWon.winNumber + " a gracz nr " + playerLoser.playerNumber + " ma wygranych: " + playerLoser.winNumber);
+        }
+        public static void Gameloop(Player player1, Player player2)
+        {
+            Console.Write("tura gracza 1: \n");
+            bool gameover = false;
+            player1.ChooseYourShip(player1);
+
+
+            Console.Write("Koniec tury gracza 1: ");
+            Console.ReadLine();
+            Console.Clear();
+
+            Console.Write("tura gracza 2: \n");
+
+            player2.ChooseYourShip(player2);
+
+            Console.Write("Koniec tury gracza 2: ");
+            Console.ReadLine();
+            Console.Clear();
+
+            while (!gameover)
+            {
+                Console.Write("tura gracza 1: \n");
+                gameover = player1.Attack(player2);
+                if (gameover)
+                {
+                    Program.Gameover(player1, player2);
+                    Program.AskIfStartAgain(player1, player2);
+                }
+                Console.ReadLine();
+                Console.Clear();
+                Console.Write("tura gracza 2: \n");
+                player2.Attack(player1);
+                if (gameover)
+                {
+                    Program.Gameover(player2, player1);
+                    Program.AskIfStartAgain(player1, player2);
+                }
+                Console.ReadLine();
+                Console.Clear();
+            }
+
+
+        }
+        public static void AskIfStartAgain(Player player1, Player player2)
+        {
+            int winCount1 = player1.winNumber;
+            int winCount2 = player2.winNumber;
+            player1 = new Player(player1.playerNumber);
+            player1.winNumber = winCount1;
+            player2 = new Player(player2.playerNumber);
+            player2.winNumber = winCount2;
+            Console.WriteLine("Kontynuuj");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("Czy chcesz zagrac ponownie? T - tak, F - nie");
+            string input = Console.ReadLine();
+            if (input.ToLower() == "t")
+            {
+                Gameloop(player1, player2);
+                
+            }
+            else if (input.ToLower() == "f")
+            {
+                Console.WriteLine("To koniec zegnam.");
+                
+            }
+            else
+            {
+                Console.WriteLine("Nie ma takiej odpowiedzi bambiku ^^");
+                AskIfStartAgain(player1, player2);
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Player player1 = new Player(1);
+            Player player2 = new Player(2);
+
+            Program.Gameloop(player1, player2);
+        }
+    }
+}
